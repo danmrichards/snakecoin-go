c68e9ea5d595fe322799e422e0864f8d32ceec07
Dan Richards <dan.m.richards@gmail.com>
2017-12-31 15:07:54 +0000

    V1 proof of concept block chain.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SnakeCoin Go
-Golang implementation of the _tiniest blockchain_ as described https://medium.com/crypto-currently/lets-build-the-tiniest-blockchain-e70965a248bg
+Golang implementation of the _tiniest blockchain_ as described at 
+https://medium.com/crypto-currently/lets-build-the-tiniest-blockchain-e70965a248bg
---
+++ domain/block.go
@@ -0,0 +1,27 @@
+package domain
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/danmrichards/snakecoin-go/models"
+)
+
+// Manually creates an initial "Genesis" block to start the chain. Note
+// arbitrary data and previous block hash.
+func CreateGenesisBlock() *models.Block {
+	return models.NewBlock(0, time.Now(), []byte("Genesis Block"), []byte("0"))
+}
+
+// Creates the next block in the chain based on the previous Block.
+func NextBlock(b *models.Block) *models.Block {
+	nextIndex := b.Index
+	nextIndex++
+
+	return models.NewBlock(
+		nextIndex,
+		time.Now(),
+		[]byte(fmt.Sprintf("I'm block %d", nextIndex)),
+		b.Hash,
+	)
+}
